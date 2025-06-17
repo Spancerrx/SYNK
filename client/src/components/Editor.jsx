dce492d202c7709eff692fb575cc5ee82aa726fb
Atharva Chavan <spancerx11@gmail.com>
2025-06-18 01:20:09 +0530

    Completed logic for Code Editor feature and solved sync issues.

--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
-import { useState } from 'react';
+import { useState, useEffect, useRef } from 'react';
 import Editor from "@monaco-editor/react";
 import axios from 'axios';
+import socket from '../socket';
 
 function CodeEditor({ roomId, username }) {
     const [code, setCode] = useState('// Start coding...');
@@ -9,8 +10,19 @@ function CodeEditor({ roomId, username }) {
     const [output, setOutput] = useState('');
     const [isRunning, setIsRunning] = useState(false);
 
+    const editorRef = useRef(null);
+    const preventEmit = useRef(false);//prevents infinite emit loop
+
     const handleLanguageChange = (e) => setLanguage(e.target.value);
-    const handleEditorChange = (value) => setCode(value);
+    const handleEditorChange = (value) => {
+        setCode(value);
+        if(!preventEmit.current) {
+            socket.emit("code-change", {
+                roomId,
+                code: value,
+            })
+        }
+    };
 
     const handleRun = async () => {
         setIsRunning(true);
@@ -21,11 +33,43 @@ function CodeEditor({ roomId, username }) {
             });
             setOutput(res.data.output);
         }
-        catch {
+        catch (err) {
             setOutput(`Error: ${err.response?.data?.error || err.message}`);
         }
         setIsRunning(false);
     };
+    useEffect(() => {
+        if(!socket) return;
+        socket.emit("Join", { roomId, username });
+        const handleCodeUpdate = (incomingCode) => {
+            preventEmit.current = true;
+            setCode(incomingCode);
+
+            if(editorRef.current) editorRef.current.setValue(incomingCode);
+
+            setTimeout(() => {
+                preventEmit.current = false;
+            }, 100);
+        };
+
+        socket.on("code-update", handleCodeUpdate);
+
+        socket.on("load-code", (incomingCode) => {
+            preventEmit.current = true;
+            setCode(incomingCode);
+            if(editorRef.current) editorRef.current.setValue(incomingCode);
+            setTimeout(() => {
+                preventEmit.current = false;
+            }, 100);
+        });
+
+
+        return () => {
+            socket.off("code-update", handleCodeUpdate);
+            socket.off("load-code");
+        };
+    }, [roomId, username]);
+
 
     return(
         <div>
@@ -49,6 +93,7 @@ function CodeEditor({ roomId, username }) {
                 language={language}
                 value={code}
                 onChange={handleEditorChange}
+                onMount={(editor) => (editorRef.current = editor)} //need for sync
                 options={{
                     fontSize: 14,
                     minimap: {enabled: false},
--- server/src/socket.js
+++ server/src/socket.js
@@ -7,15 +7,22 @@ export default function initSocket(server) {
             methods: ["GET", "POST"]
         }
     });
-
+    const rooms = {};
     io.on("connection", (socket) => {
         console.log("New Client Connected", socket.id);
 
         socket.on("join", ({roomId, username}) => {
             socket.join(roomId);
             console.log(`${username} joined the room ${roomId}`);
+            if (rooms[roomId]) {
+                socket.emit("load-code", rooms[roomId]);
+            }
         });
-
+        //Real time sync
+        socket.on("code-change", ({roomId, code }) => {
+        rooms[roomId] = code;
+            socket.to(roomId).emit("code-update", code)
+        });
         socket.on("disconnect", () => {
             console.log("Socket Disconnected", socket.id)
         });
